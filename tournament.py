340a1f7701c694d10f1bffce79bc9bc8d465fb47
Ken <ng.ken84@gmail.com>
2016-04-22 22:13:18 -0700

    Fix ReportMatch Fix playerStandings

--- tournament.py
+++ tournament.py
@@ -15,7 +15,7 @@ def deleteMatches():
     """Remove all the match records from the database."""
     conn = connect()
     cursor = conn.cursor()
-    cursor.execute("DELETE FROM games")
+    cursor.execute("DELETE FROM games;")
     conn.commit()
     conn.close()
     
@@ -25,7 +25,7 @@ def deletePlayers():
     """Remove all the player records from the database."""
     conn = connect()
     cursor = conn.cursor()
-    cursor.execute("DELETE FROM players")
+    cursor.execute("DELETE FROM players;")
     conn.commit()
     conn.close()
 
@@ -33,7 +33,7 @@ def countPlayers():
     """Returns the number of players currently registered."""
     conn = connect()
     cursor = conn.cursor()
-    cursor.execute("SELECT COUNT(*) AS NUM FROM players")
+    cursor.execute("SELECT COUNT(*) AS NUM FROM players;")
     count = cursor.fetchone()
     conn.close()
     return count[0]
@@ -52,7 +52,7 @@ def registerPlayer(name):
     """
     conn = connect()
     cursor = conn.cursor()
-    cursor.execute("INSERT into players (name) values (%s)", (name,))
+    cursor.execute("INSERT into players (name) values (%s);", (name,))
     conn.commit()
     conn.close()
     
@@ -70,15 +70,13 @@ def playerStandings():
         wins: the number of matches the player has won
         matches: the number of matches the player has played
     """
-    conn = cursor()
+    conn = connect()
     cursor = conn.cursor();
-    cursor.execute("SELECT pt.id, pt.name, count(gt.winner) as wins, "
-                   "mcv.count as matches "
-                   "from games gt "
-                   "right join players pt on pt.id = gt.winner "
-                   "inner join match_count_view mcv on mcv.id = pt.id "
-                   "group by gt.winner, pt.id, mcv.count"
-                   "order by wins desc ")
+    cursor.execute(
+        "SELECT wcv.id, wcv.name, wcv.wins, mcv.match_count as matches "
+        "from win_count_view wcv "
+        "inner join match_count_view mcv on wcv.id = mcv.id "
+        "order by wins desc ")
     retval = cursor.fetchall()
     conn.close()
     return retval
@@ -92,7 +90,13 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
- 
+    conn = connect()
+    cursor = conn.cursor()
+    cursor.execute(
+        "INSERT into games (player_one, player_two, winner) values "
+        "(%s, %s, %s); ", (winner, loser, winner))
+    conn.commit()
+    conn.close()
  
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
